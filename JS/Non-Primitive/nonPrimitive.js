67218a7fad401e7dfa146f21341870f78bb81ce8
Goutham <goutham25625@gmail.com>
2026-06-14 16:42:30 +0530

    New day New content

--- JS/Non-Primitive/nonPrimitive.js
+++ JS/Non-Primitive/nonPrimitive.js
@@ -1,4 +1,17 @@
-let ipl = ["CSK", "MI", "KKR", "KXI", "DC", "LSG", "GT", "RR", "SRH", "RCB"]
-console.log(ipl);
-console.log(ipl[0]);
-console.log(ipl.length);
+// let ipl = ["CSK", "MI", "KKR", "KXI", "DC", "LSG", "GT", "RR", "SRH", "RCB"]
+// console.log(ipl);
+// console.log(ipl[0]);
+// console.log(ipl.length);
+
+// let profile = {name: `Messi`, age: 38, team:`Argentina`, club:`Inter Miami`}
+// console.log(profile.name);
+// console.log(profile.age);
+// console.log(profile.team);
+// console.log(profile.club);
+
+let p1 = {name:`One plus`, price: 54000}
+let p2 = {name:`Samsung`, price: 75800}
+let p3 = {name:`Iphone`, price: 120000}
+let p4 = {name:`Redmi`, price: 12000}
+let product_details =[p1, p2, p3, p4]
+console.log(product_details[1].name);
